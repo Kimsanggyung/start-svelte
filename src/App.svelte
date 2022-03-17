0943cd6e0b3df7c58393def4e1071e0dda858cb3
Kim sanggyung <ksang0906@gmail.com>
2022-03-17 14:22:22 +0900

    alt 사용 사용자의 환경고려

--- src/App.svelte
+++ src/App.svelte
@@ -60,7 +60,7 @@
 		{#if logged.loggedIn}
 			<div class="loggedIn">
 				<h1>환영합니다 {userName} 회원님</h1>
-				<img src="https://ww.namu.la/s/91986ffc01b6136fb453c4ff8b3e63adf90e525e6b10e1643058339c924830276d1fc646c2ac918d1f31e83c59308ea232acb64afb280dabeee0afc024b9dd3063662e1cc7dd5e8191c10ae35b0c811b">
+				<img src="https://ww.namu.la/s/91986ffc01b6136fb453c4ff8b3e63adf90e525e6b10e1643058339c924830276d1fc646c2ac918d1f31e83c59308ea232acb64afb280dabeee0afc024b9dd3063662e1cc7dd5e8191c10ae35b0c811b" alt=" Lee Jae-yong">
 				<button on:click={toggle}>LogOut</button>
 			</div>
 		{/if}
