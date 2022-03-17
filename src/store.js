8fa2e812b08bf54870e15bb6a57b9452294b5064
Kim sanggyung <ksang0906@gmail.com>
2022-03-17 14:53:04 +0900

    변수추가 생년월일 변수를 추가하고 내보내기

--- src/store.js
+++ src/store.js
@@ -2,4 +2,6 @@ import { writable } from "svelte/store";
 
 export let name = writable('이씨')
 
+export let birth = writable('1968년 6월 23일')
+
 export let idPicture = writable('https://ww.namu.la/s/91986ffc01b6136fb453c4ff8b3e63adf90e525e6b10e1643058339c924830276d1fc646c2ac918d1f31e83c59308ea232acb64afb280dabeee0afc024b9dd3063662e1cc7dd5e8191c10ae35b0c811b')
