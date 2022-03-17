5f176a18cde8130781fbd4f91e7520c7adb91928
Kim sanggyung <ksang0906@gmail.com>
2022-03-17 14:26:09 +0900

    내보낼 변수 선언 이미주소를 값에 넣어 내보내기

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
 import { writable } from "svelte/store";
 
 export let name = writable('김씨')
+
+export let idPicture = writable('https://ww.namu.la/s/91986ffc01b6136fb453c4ff8b3e63adf90e525e6b10e1643058339c924830276d1fc646c2ac918d1f31e83c59308ea232acb64afb280dabeee0afc024b9dd3063662e1cc7dd5e8191c10ae35b0c811b')
