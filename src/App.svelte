46531ad482cd3fd0f7e9228e78f2cd5212fd0ba8
Kim sanggyung <ksang0906@gmail.com>
2022-03-17 17:21:50 +0900

    세미클론 추가 오타 수정 함수에 set추가 버튼에 이벤트추가

--- src/App.svelte
+++ src/App.svelte
@@ -13,8 +13,8 @@
 	const userId = 'a';
  	const passWord = 123;
 
-	const userJ = 'b'
-	const passWordJ =12
+	const userJ = 'b';
+	const passWordJ = 12;
  
 	let inputId = ''
 	let inputPass = ''
@@ -38,11 +38,12 @@
 
 	 function logInJ(){
 		if(
-			userId == inputId,
-			passWord == inputPass
+			userJ == inputId,
+			passWordJ == inputPass
 		){
 			logged.loggedIn = !logged.loggedIn,
 			console.log('로그인 성공')
+			name.set('정씨')
 		}
 		else{
 			document.getElementById('error').innerHTML = "아이디 또는 비밀번호가 옳지않습니다."
@@ -61,7 +62,7 @@
 			<div class="loggedIn">
 				<input bind:value={inputId} type="text" placeholder='아이디'> 
 				<input bind:value={inputPass} type="password" placeholder='비밀번호'> 
-				<button on:click={logIn}>로그인</button>
+				<button on:click={logIn} on:ckick={logInJ}>로그인</button>
 				<br/>
 				<span id=error></span>
 			</div>
