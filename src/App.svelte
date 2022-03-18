d8e3361443a24b9121b6fc9657bba5c28d101700
Kim sanggyung <ksang0906@gmail.com>
2022-03-18 12:36:30 +0900

    들여쓰기 문제 github에서 들어쓰기가 제대로 표현되지 않은 부분 수정

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,20 @@
 <script>
-import { onDestroy } from 'svelte';
 
+	import { onDestroy } from 'svelte';
+
+	import { time } from './main.js';
 	
-    import { time } from './main.js';
+	import { name, idPicture, birth, pictureAlt } from "./store.js";
 	
-    import { name, idPicture, birth, pictureAlt } from "./store.js";
-	
-    const formatter = new Intl.DateTimeFormat('ko', {
+	const formatter = new Intl.DateTimeFormat('ko', {
 		hour24: true,
 		hour: 'numeric',
 		minute: '2-digit',
 		second: '2-digit'
 	});
 	
-    const userId = 'a';
- 	const passWord = 123;
+	const userId = 'a';
+	const passWord = 123;
 
 	const userNo2 = 'b';
 	const passNo2 = 345;
@@ -22,39 +22,35 @@ import { onDestroy } from 'svelte';
 	let inputId = ''
 	let inputPass = ''
 	
-    let logged = {loggedIn: false}
+	let logged = {loggedIn: false}
 	
-    let toggle = () => logged.loggedIn = !logged.loggedIn;
+	let toggle = () => logged.loggedIn = !logged.loggedIn;
  	
-    function logIn(){
-		 if(
+	function logIn(){
+		if(
 			 userId == inputId,
 			 passWord == inputPass
-		 ){
+		){
 			logged.loggedIn = !logged.loggedIn,
 			console.log('로그인 성공')
-		 }
+		}
 
-		 else if(
+		else if(
 			 userNo2 == inputId,
 			 passNo2 == inputPass
-		 ){
-			 name.set('정씨')
-			 birth.set('1968년 9월 19일')
-			 idPicture.set('https://w.namu.la/s/2b83516ecb2360d95faa10f85763ad59ade34ede897311db5fc3a40256d7f1dd855bbf8eddb57c9fff4d4a24d070fd5ad625181926ea33915713e2351be1486fd1d510c24e5d05bc1f220991ec051010')
-			 pictureAlt.set('Chung Yongjin')
-			 logged.loggedIn = !logged.loggedIn
-			 console.log('로그인 성공')
-		 }
+		){
+			name.set('정씨')
+			birth.set('1968년 9월 19일')
+			idPicture.set('https://w.namu.la/s/2b83516ecb2360d95faa10f85763ad59ade34ede897311db5fc3a40256d7f1dd855bbf8eddb57c9fff4d4a24d070fd5ad625181926ea33915713e2351be1486fd1d510c24e5d05bc1f220991ec051010')
+			pictureAlt.set('Chung Yongjin')
+			logged.loggedIn = !logged.loggedIn
+			console.log('로그인 성공')
+		}
 
-		 else{
-			 document.getElementById('error').innerHTML = "아이디 또는 비밀번호가 옳지않습니다."
-		 }
-	 }
-
-	 idPicture.subscribe((value) => {
-		 console.log(value)
-	 })
+		else{
+			document.getElementById('error').innerHTML = "아이디 또는 비밀번호가 옳지않습니다."
+		}
+	}
 	
 </script>
 
@@ -96,5 +92,5 @@ import { onDestroy } from 'svelte';
 		width: 175px;
 		height: 269px;
 	}
-    
+
 </style>
