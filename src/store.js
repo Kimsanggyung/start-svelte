057611a216bd66adb5babb76a3c3cb5bc3f21818
Kim sanggyung <ksang0906@gmail.com>
2022-03-23 15:53:03 +0900

    초기값을 빈값으로

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { writable } from "svelte/store";
 
-export let name = writable('이씨')
+export let name = writable(0)
 
-export let birth = writable('1968년 6월 23일')
+export let birth = writable(0)
 
-export let idPicture = writable('https://ww.namu.la/s/91986ffc01b6136fb453c4ff8b3e63adf90e525e6b10e1643058339c924830276d1fc646c2ac918d1f31e83c59308ea232acb64afb280dabeee0afc024b9dd3063662e1cc7dd5e8191c10ae35b0c811b')
+export let idPicture = writable(0)
 
-export let pictureAlt = writable('Lee Jae-yong')
+export let pictureAlt = writable(0)
