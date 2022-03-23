11fbefe72c89f2dca6317d13d3a016421195e8d6
Kim sanggyung <ksang0906@gmail.com>
2022-03-23 16:49:56 +0900

    변수 함수 선언 체크 상태를 확인하기 위한 변수 상태를 관리하기 위한 함수 선언

---
+++ src/Setid.svelte
@@ -0,0 +1,9 @@
+<script>
+
+    let check = {checked: false}
+
+    const CLICKCHECK =  check.checked = !check.checked
+
+</script>
+
+<label><input type="checkbox" id="id" on:click={CLICKCHECK}>아이디 저장</label>
