d35d82ac4c58bff91409f3e2e37ea70e55c265b8
Kim sanggyung <ksang0906@gmail.com>
2022-03-23 11:05:33 +0900

    화면에 그려질 html추가 사용자 정보 변경을 위한 input button

--- src/editmode.svelte
+++ src/editmode.svelte
@@ -22,3 +22,10 @@
  	
 	const reflash = () => localStorage.clear();
 </script>
+
+<input bind:value={inputName} type="text" placeholder='이름을 입력해 주세요.'> 
+<input bind:value={inputBrith} type="text" placeholder='생년월일을 입력해 주세요'>
+<input bind:value={inputPicture} type="text" placeholder='시진주소를 입력해 주세요.'>
+<input bind:value={inputAlt} type="text" placeholder="사진소개를 써주세요.">
+<button on:click={change} on:click={editToggle}>등록</button>
+<button on:click={reflash}></button>
