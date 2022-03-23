71756ee43eb3d438a03f60971a918ef9ea40a257
Kim sanggyung <ksang0906@gmail.com>
2022-03-23 13:25:19 +0900

    조건문 선언 change함수에 editmode toggle 기능을 추가하고 editToggle 함수제거 조건에 맞을 떄만 화면에 표시 되도록

--- src/editmode.svelte
+++ src/editmode.svelte
@@ -9,8 +9,6 @@
 	
 	let editMode = {editModeIn: false}
 
-	const editToggle = () => editMode.editModeIn = !editMode.editModeIn
-
 	const change = () => {
 		name.set(inputName)
 		birth.set(inputBrith)
@@ -21,14 +19,19 @@
 		localStorage.setItem('brith', (inputBrith))
 		localStorage.setItem('img', (inputPicture))
 		localStorage.setItem('alt', (inputAlt))
+        editMode.editModeIn = !editMode.editModeIn
 	}
  	
 	const reflash = () => localStorage.clear();
 </script>
 
-<input bind:value={inputName} type="text" placeholder='이름을 입력해 주세요.'> 
-<input bind:value={inputBrith} type="text" placeholder='생년월일을 입력해 주세요'>
-<input bind:value={inputPicture} type="text" placeholder='시진주소를 입력해 주세요.'>
-<input bind:value={inputAlt} type="text" placeholder="사진소개를 써주세요.">
-<button on:click={change} on:click={editToggle}>등록</button>
-<button on:click={reflash}>초기화</button>
+{#if editMode.editModeIn}
+    
+    <input bind:value={inputName} type="text" placeholder='이름을 입력해 주세요.'> 
+    <input bind:value={inputBrith} type="text" placeholder='생년월일을 입력해 주세요'>
+    <input bind:value={inputPicture} type="text" placeholder='시진주소를 입력해 주세요.'>
+    <input bind:value={inputAlt} type="text" placeholder="사진소개를 써주세요.">
+    <button on:click={change}>등록</button>
+    <button on:click={reflash}>초기화</button>
+
+{/if}
