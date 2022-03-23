3d402b102fec63668b4cd46e012c60bd82818f93
Kim sanggyung <ksang0906@gmail.com>
2022-03-23 10:57:07 +0900

    변수 함수 선언 변경할 정보를 입력받을 함수 로그인 상태를 확인할 함수 로그인 상태를 관리 할 함수 정부를 변경시켜 줄 함수 추가

---
+++ src/editmode.svelte
@@ -0,0 +1,24 @@
+<script>
+    let	inputName = ''
+	let inputBrith = ''
+	let	inputPicture = ''
+	let inputAlt =''
+	
+	let editMode = {editModeIn: false}
+
+	const editToggle = () => editMode.editModeIn = !editMode.editModeIn
+
+	const change = () => {
+		name.set(inputName)
+		birth.set(inputBrith)
+		idPicture.set(inputPicture)
+		pictureAlt.set(inputAlt)
+		console.log(name)
+		localStorage.setItem('name', (inputName))
+		localStorage.setItem('brith', (inputBrith))
+		localStorage.setItem('img', (inputPicture))
+		localStorage.setItem('alt', (inputAlt))
+	}
+ 	
+	const reflash = () => localStorage.clear();
+</script>
