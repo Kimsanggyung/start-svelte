40fc089349e806d7fbbfce7a70739a2748ebc349
Kim sanggyung <ksang0906@gmail.com>
2022-03-23 16:41:50 +0900

    로그인시 사용자 세팅

--- src/LogIn.svelte
+++ src/LogIn.svelte
@@ -23,12 +23,26 @@
 		logged.loggedIn = !logged.loggedIn;
 		editMode.editModeIn = !editMode.editModeIn;
 	}
+	console.log(name)
+	console.log(birth)
+	console.log(idPicture)
+	console.log(pictureAlt)
+
+	let check = document.getElementById('id')
+
+	let checked = check.getAttribute("checked")
+
+	console.log(checked)
 
 	function logIn(){
 		if(
 			userId == inputId,
 			passWord == inputPass
 		){
+			name.set('이씨')
+			birth.set('1968년 6월 23일')
+			idPicture.set('https://ww.namu.la/s/91986ffc01b6136fb453c4ff8b3e63adf90e525e6b10e1643058339c924830276d1fc646c2ac918d1f31e83c59308ea232acb64afb280dabeee0afc024b9dd3063662e1cc7dd5e8191c10ae35b0c811b')
+			pictureAlt.set('Lee Jae-yong')
 			logged.loggedIn = !logged.loggedIn,
 			console.log('로그인 성공')
 		}
@@ -40,7 +54,7 @@
 			name.set('정씨')
 			birth.set('1968년 9월 19일')
 			idPicture.set('https://w.namu.la/s/2b83516ecb2360d95faa10f85763ad59ade34ede897311db5fc3a40256d7f1dd855bbf8eddb57c9fff4d4a24d070fd5ad625181926ea33915713e2351be1486fd1d510c24e5d05bc1f220991ec051010')
-			pictureAlt.set('Chung Yongjin')
+			pictureAlt.set('Chung Yong-jin')
 			logged.loggedIn = !logged.loggedIn
 			console.log('로그인 성공')
 		}
@@ -60,7 +74,7 @@
 				<input bind:value={inputPass} type="password" placeholder='비밀번호'> 
 				<button on:click={logIn}>로그인</button>
 				<br/>
-				<label><input type="checkbox">아이디 저장</label>
+				<label><input type="checkbox" id="id">아이디 저장</label>
 				<span id=error></span>
 			</div>
 		{/if}
