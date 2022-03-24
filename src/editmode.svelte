0fc966be2227dfaa02d4723bc3fa67b5538f2c70
Kim sanggyung <ksang0906@gmail.com>
2022-03-24 17:39:16 +0900

    입력창에 입력을 하지 않으면 경고창이 뜨도록

--- src/editmode.svelte
+++ src/editmode.svelte
@@ -3,14 +3,30 @@
     import { name, idPicture, birth, pictureAlt } from "./store.js";
     
     let	inputName = ''
-	let inputBrith = ''
+	let inputBirth = ''
 	let	inputPicture = ''
 	let inputAlt =''
 	
 	let editMode = {editModeIn: false}
 
 	const change = () => {
-		name.set(inputName)
+		if(inputName == '', inputBirth == '', inputPicture == '', inputAlt == ''){
+			alert('변경할 정보을 입력해 주세요.')
+		}
+		else if(inputName == '', inputBirth !== '', inputPicture !== '', inputAlt !== ''){
+			alert('변경할 이름을 입략해 주세요.')
+		}
+		else if(inputName !== '', inputBirth == '', inputPicture !== '', inputAlt !== ''){
+			alert('변경할 생년월일을 입략해 주세요.')
+		}
+		else if(inputName !== '', inputBirth !== '', inputPicture == '', inputAlt !== ''){
+			alert('변경할 사진주소을 입략해 주세요.')
+		}
+		else if(inputName == '', inputBirth !== '', inputPicture !== '', inputAlt == ''){
+			alert('변경할 사진 정보를 입력해 주세요')
+		}
+		else{
+			name.set(inputName)
 		birth.set(inputBrith)
 		idPicture.set(inputPicture)
 		pictureAlt.set(inputAlt)
@@ -20,15 +36,16 @@
 		localStorage.setItem('img', (inputPicture))
 		localStorage.setItem('alt', (inputAlt))
         editMode.editModeIn = !editMode.editModeIn
+		}
 	}
-	
+
 	const reflash = () => localStorage.clear();
 </script>
 
 {#if !editMode.editModeIn}
     
     <input bind:value={inputName} type="text" placeholder='이름을 입력해 주세요.'> 
-    <input bind:value={inputBrith} type="text" placeholder='생년월일을 입력해 주세요'>
+    <input bind:value={inputBirth} type="text" placeholder='생년월일을 입력해 주세요'>
     <input bind:value={inputPicture} type="text" placeholder='시진주소를 입력해 주세요.'>
     <input bind:value={inputAlt} type="text" placeholder="사진소개를 써주세요.">
     <button on:click={change}>등록</button>
